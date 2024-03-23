3343b2b06f1696b2060586a0fd5a6ff9c0df07ea
Priyanshu Sinha <officialpriyanshusinha@gmail.com>
2024-03-23 13:53:44 +0530

    team pic

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,17 +6,22 @@ import nimbus from "@/public/images/vehicles/Nimbus.png"
 import pegasus from "@/public/images/vehicles/Pegasus.png"
 import eurus from "@/public/images/vehicles/Eurus.png"
 import falcon from "@/public/images/vehicles/Falcon.jpg"
+import team from "@/public/images/team-min.jpeg"
+
 
 export default function About() {
 	return (
 		<Container>
-            <h1 className="text-4xl font-bold font-accent mb-4 text-primary text-center">Our Mission</h1>
-            <section className="text-justify text-lg text-slate-200 max-w-5xl mx-auto">
-            <p className="my-1 leading-snug">We are the Human Powered Vehicle Team consisting of undergraduate students who aim to give the world an alternative solution to the present-day energy crisis.</p>
-            <p className="my-1 leading-snug">We study, design, develop, and manufacture the fully-faired recumbent bikes and trikes which can reach up to the highest speeds of 60 kmph solely by human power!!! The team rigorously researches about the human-powered vehicles and uses the best possible design, the best possible techniques, and the best possible materials for manufacturing.</p>
-            <p className="my-1 leading-snug">From building fairing completely from carbon fiber to incorporating the internationally acclaimed landing gear system in the vehicle, Team Raftaar is redefining the way human-powered vehicles are perceived in India. Our unique manufacturing style, innovative design, detailed analysis, and advanced use of composites have been appreciated and awarded multiple times both in National and international competitions.</p>
-            <p className="my-1 leading-snug">With each passing year, we as a team are getting closer and closer to our ultimate ambition of bringing HPVs closer to the everyday commuters as a sustainable alternative to other conventional modes of transport.</p>
+            <h1 className="text-4xl font-bold font-accent mb-6 text-primary text-center">Our Mission</h1>
+            <div className="flex flex-col md:flex-row gap-6 items-center">
+            <Image src={team} alt="Whole Team Pic" className="w-full md:w-1/2 h-auto rounded-lg"/>
+            <section className="text-left text-lg text-slate-200 mx-auto">
+            <p className="my-2 leading-snug">We are the Human Powered Vehicle Team consisting of undergraduate students who aim to give the world an alternative solution to the present-day energy crisis.</p>
+            <p className="my-2 leading-snug">We study, design, develop, and manufacture the fully-faired recumbent bikes and trikes which can reach up to the highest speeds of 60 kmph solely by human power!!! The team rigorously researches about the human-powered vehicles and uses the best possible design, the best possible techniques, and the best possible materials for manufacturing.</p>
+            <p className="my-2 leading-snug">From building fairing completely from carbon fiber to incorporating the internationally acclaimed landing gear system in the vehicle, Team Raftaar is redefining the way human-powered vehicles are perceived in India. Our unique manufacturing style, innovative design, detailed analysis, and advanced use of composites have been appreciated and awarded multiple times both in National and international competitions.</p>
+            <p className="my-2 leading-snug">With each passing year, we as a team are getting closer and closer to our ultimate ambition of bringing HPVs closer to the everyday commuters as a sustainable alternative to other conventional modes of transport.</p>
 			</section>
+            </div>
 			<h1 className="text-4xl font-bold font-accent mt-8 mb-4 text-primary text-center">Past Vehicles</h1>
             <div className="mt-12 grid gap-6 md:gap-12 md:grid-cols-3">
             <div
